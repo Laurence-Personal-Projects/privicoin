ad8146969ad2b0ccd0b8aa9a2b1e15ec3255bb49
laurencealbano12 <laurence.albano18@gmail.com>
2025-02-27 17:38:58 +0800

    updated header animation

--- src/assets/js/components/Header.js
+++ src/assets/js/components/Header.js
@@ -1,10 +1,22 @@
-import React from "react";
-import MainLogo from '@/assets/images/privicoin-logo-@2x.webp';
+import React, { useEffect, useState } from "react";
+import MainLogo from "@/assets/images/privicoin-logo-@2x.webp";
 import Button from "@/assets/js/components/Button";
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollThreshold = document.documentElement.scrollHeight * 0.05; // 5% of page height
+            setIsScrolled(window.scrollY > scrollThreshold);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <header className="main-header tw-w-full tw-fixed tw-top-0 tw-z-[100]">
+        <header className={`main-header tw-w-full tw-fixed tw-top-0 tw-z-[100] ${isScrolled ? "is-scrolled" : ""}`}>
             <div className="tw-flex tw-items-center tw-justify-between tw-flex-wrap">
                 <a href={window.location.origin} className="header-logo tw-w-[120px] md:tw-w-[194px]">
                     <img src={MainLogo} className="header-logo-image" alt="Privicoin Logo" />
